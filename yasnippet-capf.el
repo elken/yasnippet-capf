c175346aac2f8feaa0bf1decdc74c9bed92db111
StrawberryTea <look@strawberrytea.xyz>
2024-02-20 13:00:11 -0600

    Reapply "fix: allow completing interactively without prefix (#15)"
    This reverts commit db12b55cd08b614cbba134008566e48d7faf660e.

--- yasnippet-capf.el
+++ yasnippet-capf.el
@@ -121,8 +121,9 @@ If INTERACTIVE is nil the function acts like a Capf."
   (if interactive
       (let ((completion-at-point-functions #'yasnippet-capf))
         (or (completion-at-point) (user-error "yasnippet-capf: No completions")))
-    (when (thing-at-point-looking-at "\\(?:\\sw\\|\\s_\\)+")
-      `(,(match-beginning 0) ,(match-end 0)
+    (when-let ((bounds (or (bounds-of-thing-at-point 'word)
+                      (cons (point) (point)))))
+      `(,(car bounds) ,(cdr bounds)
         ,(completion-table-with-cache
           (lambda (input)
             (yasnippet-capf-candidates input)))
